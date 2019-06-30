891da79bc15f723d05b76e71453ef0de00e87040
fr064722 <52367545+fr064722@users.noreply.github.com>
2019-06-30 14:08:47 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# aro-tools
+# tools
+
+伺服馬達專用的micro:bit積木
+
+## License
+
+MIT
+
+## Supported targets
+
+* for PXT/microbit
+(The metadata above is needed for package search.)
+
+```package
+tools=github:fr064722/aro-tools
+```
---
+++ main.ts
@@ -0,0 +1,33 @@
+//% weight=0 color=#3CB371 icon="\uf0ad" block="Tools"
+namespace tools {
+    /**
+    * 計算手臂旋轉角度，並回傳
+    */
+    //% blockId=“servo” block=“servo %servo %p %a %c”
+    //% blockGap=2 weight=0 blockExternalInputs=true
+    export function servo(servo: number, p:number): number {
+        return length*width
+    }
+    /**
+    * 計算長方形面積，不回傳，只顯示在LED
+    */
+    //% blockId="ledOfRectangle" block="show area of rectangle length %length|width %width"
+    //% blockGap=2 weight=1
+    export function ledOfRectangle(length: number, width:number): void {
+        basic.showNumber(length*width)
+    }
+}
+export function Servo(index: Servos, p: number, a: number, c: number): void {
+        if (!initialized) {
+            initPCA9685()
+        }
+        // 50hz: 20,000 us
+        let v_us = ((degree - 90) * 20 / 3 + 1500) // 0.6 ~ 2.4
+        let value = v_us * 4096 / 20000
+        let d = c/20
+        let b=(p-a)/d
+        for (let d ; d > 0; d—) {
+            a = a+b;
+        }
+        setPwm(index + 7, 0, a)           
+    }
